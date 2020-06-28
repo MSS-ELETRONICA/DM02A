1827fa03efacfdce408834c75b92fd1995102aa4
MSS ELETRÔNICA <60515822+MSS-ELETRONICA@users.noreply.github.com>
2020-06-27 22:14:38 -0300

    Add files via upload
    Código exemplo para Rasberry criado e fornecido pelo nosso amigo Leonardo Oliveira - knowdbtech (https://github.com/knowdbtech). Obrigado pela contribuição Leonardo.

---
+++ raspberry_envia_nivel_dm02a.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time,sys
+
+if len (sys.argv) <= 1: sys.exit('Parametro entre 0 e 70 eh necessario!')
+
+nivel = 0
+
+try:
+    nivel = int(sys.argv[1])
+except:
+  print("O parametro deve ser numerico")
+  sys.exit()
+
+if nivel < 0 or nivel > 70: sys.exit('Parametro fora do range 0-70')
+
+usleep = lambda x: time.sleep(x/1000000.0)
+
+refval = int(sys.argv[1]) * 500;
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(10, GPIO.OUT)
+GPIO.output(10,0)
+
+
+GPIO.setup(9, GPIO.OUT)
+
+GPIO.output(9,GPIO.LOW)
+usleep(refval)
+GPIO.output(9,GPIO.HIGH)
